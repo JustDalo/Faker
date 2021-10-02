125c87002fd59f755675004b289b02cb078e7697
JustDalo <shishlodan@mail.ru>
2021-10-02 10:44:33 +0300

    faker base

--- Faker/Faker.cs
+++ Faker/Faker.cs
@@ -3,28 +3,43 @@ using System.Collections.Generic;
 using System.Dynamic;
 using Faker.ValueGenerator;
 using Faker.ValueGenerator.PrimitiveTypes;
+using Faker.ValueGenerator.PrimitiveTypes.LogicalType;
 
 namespace Faker
 {
     public class Faker
     {
-        private Dictionary<Type, IPrimitiveTypeGenerator> _generators;
+        private Dictionary<Type, IPrimitiveTypeGenerator> _primitiveTypeGenerators;
         
         public Faker()
         {
-            
+            _primitiveTypeGenerators = new Dictionary<Type, IPrimitiveTypeGenerator>()
+            {
+                {typeof(Boolean), new BooleanGenerator()},
+                
+            };
         }
         
-        public T Create<T>() where T : class
+        public T Create<T>()
         {
             var type = typeof(T);
-               
-            return null;
+            return (T) Create(type);
         }
 
-        private void Create(Type objectType)
+        private object Create(Type objectType)
         {
+            object generatedType = new object();
+            if (_primitiveTypeGenerators.TryGetValue(objectType, out IPrimitiveTypeGenerator primitiveTypeGenerator))
+            {
+                generatedType = primitiveTypeGenerator.Generate();
+            }
+            else
+            {
+                generatedType = null;
+            }
             
+
+            return generatedType;
         }
     }
 }
